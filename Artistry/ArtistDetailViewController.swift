4b8851c69b7d76005cb7509bdf2c509b655a115a
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-29 16:15:56 +0700

    complete project

--- Artistry/ArtistDetailViewController.swift
+++ Artistry/ArtistDetailViewController.swift
@@ -33,6 +33,8 @@ class ArtistDetailViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     title = selectedArtist.name
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 300
   }
 }
 
@@ -42,12 +44,47 @@ extension ArtistDetailViewController: UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! WorkTableViewCell
     
     let work = selectedArtist.works[indexPath.row]
-    cell.textLabel?.text = work.info
+    
+    cell.workTitleLabel.text = work.title
+    cell.workImageView.image = work.image
+    
+    cell.workTitleLabel.backgroundColor = UIColor(white: 204/255, alpha: 1)
+    cell.workTitleLabel.textAlignment = .center
+    cell.moreInfoTextView.textColor = UIColor(white: 114/255, alpha: 1)
+    cell.selectionStyle = .none
+    
+    cell.moreInfoTextView.text = work.isExpanded ? work.info : moreInfoText
+    cell.moreInfoTextView.textAlignment = work.isExpanded ? .left : .center
     
     return cell
+  }
+}
+
+
+extension ArtistDetailViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    // 1
+    guard let cell = tableView.cellForRow(at: indexPath) as? WorkTableViewCell else { return }
+    
+    var work = selectedArtist.works[indexPath.row]
+    
+    // 2
+    work.isExpanded = !work.isExpanded
+    selectedArtist.works[indexPath.row] = work
+    
+    // 3
+    cell.moreInfoTextView.text = work.isExpanded ? work.info : moreInfoText
+    cell.moreInfoTextView.textAlignment = work.isExpanded ? .left : .center
+    
+    // 4
+    tableView.beginUpdates()
+    tableView.endUpdates()
+    
+    // 5
+    tableView.scrollToRow(at: indexPath, at: .top, animated: true)
   }
 }
 
--- Artistry/ArtistListViewController.swift
+++ Artistry/ArtistListViewController.swift
@@ -30,6 +30,16 @@ class ArtistListViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 140
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    NotificationCenter.default.addObserver(forName: .UIContentSizeCategoryDidChange, object: .none, queue: OperationQueue.main) { [weak self] _ in
+      self?.tableView.reloadData()
+    }
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -46,11 +56,21 @@ extension ArtistListViewController: UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArtistTableViewCell
     
     let artist = artists[indexPath.row]
+    cell.bioLabel.text = artist.bio
+    cell.artistImageView.image = artist.image
+    cell.nameLabel.text = artist.name
     
-    cell.textLabel?.text = artist.bio
+    cell.bioLabel.textColor = UIColor(white: 114/255, alpha: 1)
+    cell.nameLabel.backgroundColor = UIColor(red: 1, green: 152/255, blue: 0, alpha: 1)
+    cell.nameLabel.textColor = UIColor.white
+    cell.nameLabel.textAlignment = .center
+    cell.selectionStyle = .none
+    
+    cell.nameLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+    cell.bioLabel.font = UIFont.preferredFont(forTextStyle: .body)
     
     return cell
   }
